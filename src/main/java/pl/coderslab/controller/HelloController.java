b77d7259da9c3f2aff5303dfd4fb3bb39493ff23
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-05-23 14:15:10 +0200

    Zadania Spring dzien 2

--- src/main/java/pl/coderslab/controller/HelloController.java
+++ src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,9 +1,12 @@
 package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalTime;
 
 @Controller
 public class HelloController {
@@ -19,5 +22,20 @@ public class HelloController {
     public String hello(@PathVariable String firstName, @PathVariable String lastName) {
         return "Witaj " + firstName + " " + lastName;
     }
+
+    @GetMapping("/helloView")
+    public String helloView(Model model) {
+        String color = "black";
+        String backgroundColor = "white";
+        LocalTime time = LocalTime.now();
+        if (time.getHour() >= 20 || time.getHour() <= 8) {
+            color = "white";
+            backgroundColor = "black";
+        }
+        model.addAttribute("color", color);
+        model.addAttribute("backgroundColor", backgroundColor);
+        return "home";
+    }
+
 
 }
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <style type="text/css">
+        body {
+            color: ${color};
+            background-color: ${backgroundColor};
+        }
+    </style>
+</head>
+<body>
+<h1>Witaj w widoku</h1>
+</body>
+</html>
