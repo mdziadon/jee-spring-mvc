09cd153cd2f2a9a7c1b458bd2d3d13d92becce2a
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-05-23 12:06:54 +0200

    Zadania Spring dzien 2

--- src/main/java/pl/coderslab/controller/HelloController.java
+++ src/main/java/pl/coderslab/controller/HelloController.java
@@ -2,6 +2,7 @@ package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +12,12 @@ public class HelloController {
     @ResponseBody
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello/{firstName}/{lastName}")
+    @ResponseBody
+    public String hello(@PathVariable String firstName, @PathVariable String lastName) {
+        return "Witaj " + firstName + " " + lastName;
     }
 
 }
