123eed782bca549e87b069b9314016a0b1f179fa
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-05-23 15:11:28 +0200

    Zadania Spring dzien 2

--- src/main/java/pl/coderslab/controller/MultiplyController.java
+++ src/main/java/pl/coderslab/controller/MultiplyController.java
@@ -3,6 +3,7 @@ package pl.coderslab.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,6 +13,13 @@ public class MultiplyController {
     public String multiply(Model model, @RequestParam(defaultValue = "10") int size) {
         model.addAttribute("size", size);
         return "multiply";
+    }
+
+    @GetMapping("/paramMultiply/{rows}/{cols}")
+    public String paramMultiply(@PathVariable int rows, @PathVariable int cols, Model model){
+        model.addAttribute("rows", rows);
+        model.addAttribute("cols", cols);
+        return "paramMultiply";
     }
 
 
